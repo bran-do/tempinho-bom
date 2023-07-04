54071ca23bbfcbcc582a121b58339cfd79251597
Felipe Brandão <brandaofelipebrandao@gmail.com>
2023-07-04 04:11:57 -0300

    Começando o projeto

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,7 +1,12 @@
 // Remova os comentários a medida que for implementando as funções
+const TOKEN = 'd14d5ccb65ca4874b8103716230407';
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+export const searchCities = (TERMO_DE_BUSCA) => {
+    fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${TERMO_DE_BUSCA}`)
+    .then((res) => res.json())
+    .then((data) => {
+        return data;
+    })
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
