2277c74c92be1857f407858908ff0737806e6212
Felipe Brandão <brandaofelipebrandao@gmail.com>
2023-07-04 04:32:06 -0300

    1. Implemente a função searchCities

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -2,13 +2,16 @@
 const TOKEN = 'd14d5ccb65ca4874b8103716230407';
 
 export const searchCities = (TERMO_DE_BUSCA) => {
-    fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${TERMO_DE_BUSCA}`)
+  fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${TERMO_DE_BUSCA}`)
     .then((res) => res.json())
     .then((data) => {
-        return data;
-    })
+      if (data.length === 0) {
+        window.alert('Nenhuma cidade encontrada');
+      }
+      return data;
+    });
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
-//   seu código aqui
+  //   seu código aqui
 };
